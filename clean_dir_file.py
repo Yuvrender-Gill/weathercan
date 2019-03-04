8c85b7960a6a54632509a4a52a1c2b1e9ee083ca
Yuvrender Gill <yuvrender.gill@mail.utoronto.ca>
2019-03-04 00:37:19 -0500

    Filters

--- clean_dir_file.py
+++ clean_dir_file.py
@@ -4,6 +4,10 @@ def main():
     df =  pd.read_csv("./stations_dir/raw_directory.csv", skiprows=3)
     df.to_csv("./stations_dir/directory.csv", sep=',')
 
+    df1 = df[(df['HLY First Year'].notnull()) & (df['Province'] == 'ONTARIO') & (df['HLY First Year'] >= 2000)]
+    df1.to_csv("./stations_dir/2000.csv", sep=',')
+
+
 if __name__ == "__main__":
     main()
 
